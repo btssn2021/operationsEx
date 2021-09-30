d31c73320a4ebde8ddaffea2c2c223b4ea949406
eric <eric.thomas@formation-industries-fc.fr>
2021-09-30 16:15:18 +0200

    nb aléatoire

--- main.c
+++ main.c
@@ -4,7 +4,7 @@
 
 int main() {
     //nombre alétoire C srand rand
-    //initilaisation du moteur alétoire avec srand
+    //initialisation du moteur alétoire avec srand
     srand(time(NULL));
     int nbaleatoire=rand()%10+1; //rencoi un nombre aléatoire entre 1 et 10
     printf("%d",nbaleatoire);
